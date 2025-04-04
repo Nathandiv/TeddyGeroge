042ca31450c1cab3b730223e9acff9837f0c870a
Nathandiv <k.n.hlungwani@gmail.com>
2025-04-04 09:51:11 +0200

    add About page with content, styling, and team information; update navbar links

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,11 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
-
+  team = [
+    {
+      name: 'John Doe',
+      position: 'Founder & CEO',
+      image: 'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
+    },
+    {
+      name: 'Jane Smith',
+      position: 'Design Director',
+      image: 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
+    }
+  ];
 }
