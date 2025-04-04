7692c67ac8d6e6486de5ef9fd85962c7e7cba5ed
Nathandiv <k.n.hlungwani@gmail.com>
2025-04-04 12:15:28 +0200

    add AOS library for animations; update styles and navbar for consistency

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,16 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
 
+import 'aos/dist/aos.css';
+import AOS from 'aos';
+
+AOS.init();
+
+
+AOS.init({
+  duration: 800,
+  once: true
+});
+
 bootstrapApplication(AppComponent, appConfig)
   .catch(err => console.error(err));
